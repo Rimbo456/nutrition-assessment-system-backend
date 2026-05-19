370a7365de449f9edefca6fa339b08cb92b08031
Rimbo456 <134587817+Rimbo456@users.noreply.github.com>
2026-05-19 14:34:15 +0700

    Fix formatting issues in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ app.get('/', (req, res) => {
     res.send('Hello')
 })
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log(`Server running at ${process.env.PORT || 3000}`)
 })
